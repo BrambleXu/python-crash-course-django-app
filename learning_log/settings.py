a54593c4c19d597f0b34634eb072c8c6fd775a5e
Bramble Xu <liangxu006@gmail.com>
2018-12-07 11:45:33 +0900

    19.2.1

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # My App
     'learning_logs',
+    'users',
 ]
 
 MIDDLEWARE = [
--- learning_log/urls.py
+++ learning_log/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('', include('learning_logs.urls')),
-
 ]
 
