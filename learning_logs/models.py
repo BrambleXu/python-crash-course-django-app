c5a7e51849d7f4ac75e5957e9671c427f2973359
BrambleXu <liangxu006@gmail.com>
2018-12-08 22:52:21 +0900

    19.3.2

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
     """用户学习的主题"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """返回模型的字符串表示"""
